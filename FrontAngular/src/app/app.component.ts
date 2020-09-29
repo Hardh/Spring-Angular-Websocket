99f833926d071afca0bb27c26ca5ebef15c3127f
Alisson Dias <alisson.dias@iteris.com.br>
2020-09-29 00:07:07 -0300

    [master] Funcionando

--- FrontAngular/src/app/app.component.ts
+++ FrontAngular/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WebSocketAPI } from './WebSocketAPI';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'FrontAngular';
+  webSocketAPI: WebSocketAPI;
+  greeting: any;
+  name: string;
+
+  ngOnInit(): void {
+    this.webSocketAPI = new WebSocketAPI(new AppComponent());
+  }
+
+  connect() {
+    this.webSocketAPI._connect();
+  }
+
+  disconnect() {
+    this.webSocketAPI._disconnect();
+  }
+
+  sendMessage() {
+    this.webSocketAPI._send(this.name);
+  }
+
+  handleMessage(message) {
+    this.greeting = message;
+  }
 }
